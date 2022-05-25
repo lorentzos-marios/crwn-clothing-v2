e6e48fed8ed64119677431b533285f58b3197c0c
lorentzos-marios <102953740+lorentzos-marios@users.noreply.github.com>
2022-05-25 15:05:18 +0300

    Update App.js

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -9,7 +9,7 @@ const Shop = () => {
 const App = () => {
   return (
     <Routes>
-     <Route path="/home" element={<Home/>}>
+     <Route path="home" element={<Home/>}>
       <Route path="shop" element={<Shop/>} />
    {/* /home/shop*/}
    </Route>
