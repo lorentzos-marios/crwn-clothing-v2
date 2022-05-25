375e4e08d4fd8ccd30e6ff8cd6d32893831166e3
lorentzos-marios <102953740+lorentzos-marios@users.noreply.github.com>
2022-05-25 16:00:56 +0300

    just a router change

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,6 +1,17 @@
-import { Routes,Route } from "react-router-dom";
+import { Routes,Route,Outlet } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
+
+const Navigation = () => {
+  return (
+     <div>
+       <div>
+        <h1>I am the navigation bar </h1>
+       </div>
+       <Outlet/>
+     </div>
+   )
+}
 
 const Shop = () => {
    return <h1>I am the shop page</h1>
@@ -9,10 +20,10 @@ const Shop = () => {
 const App = () => {
   return (
     <Routes>
-     <Route path="home" element={<Home/>}>
-      <Route path="shop" element={<Shop/>} />
-   {/* /home/shop*/}
-   </Route>
+    <Route path="/" element={<Navigation/>}>
+     <Route index element={<Home/>} />
+     <Route path="shop" element={<Shop/>} />
+     </Route>
    </Routes>
   );
 };
