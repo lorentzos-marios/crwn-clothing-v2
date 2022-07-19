2941816a8c65820cf61ca483cd540302ddaffdda
lorentzos-marios <102953740+lorentzos-marios@users.noreply.github.com>
2022-07-19 17:39:38 +0300

    dsadasda

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -10,6 +10,7 @@ import Authentication from "./routes/authentication/authentication.component";
 
 import Shop from "./routes/shop/shop.component";
 
+import Checkout from "./routes/checkouts/checkouut.component";
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
      <Route index element={<Home/>} />
      <Route path="shop" element={<Shop/>} />
      <Route path="auth" element={<Authentication/>} />
+     <Route path="checkout" element={<Checkout/>} />
      </Route>
    </Routes>
   );
